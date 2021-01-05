d1edbe7d66f3a4bc261c0053750cc99478eb2bba
astraeus14-ctrl <76985247+astraeus14-ctrl@users.noreply.github.com>
2021-01-04 23:34:32 -0500

    Delete CfgRespawnInventory.hpp

--- config/CfgRespawnInventory.hpp
+++
@@ -1,4 +0,0 @@
-// Respawn Inventories
-// Used for giving a unit a custom inventory upon respawn.
-// Mostly only used with the "MenuInventory" respawn template.
-// https://community.bistudio.com/wiki/Arma_3_Respawn#Respawn_Templates
